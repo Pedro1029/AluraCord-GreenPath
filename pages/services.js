f36e6f418fd2465e21f2bd59d376b1aa2b6d0b4c
Pedro1029 <pedrosantanaalves@gmail.com>
2022-01-31 21:20:50 -0300

    teste

--- pages/services.js
+++
@@ -1,9 +0,0 @@
-export async function getUserInformationsGithub(username) {
-
-        const url = `https://api.github.com/users/${username}`
-        const response = await fetch(url);
-        const resultado = await response.json();
-        console.log(resultado)
-
-        return resultado
-}
